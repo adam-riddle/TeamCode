07c6d45eb07190b43d60e844a7d93f95b7ea52d7
DanielArnett <arnett.daniel@gmail.com>
2016-10-10 08:53:44 -0400

    Created SteelSerpentsRobot object to consolidate robot-specific code. Documented the working Mecanum drive. Needs tested.

--- SkateboardTeleOp.java
+++ SkateboardTeleOp.java
@@ -2,231 +2,63 @@ package org.firstinspires.ftc.teamcode;
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceReader;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "SkateboardTeleOp", group = "TeleOp")
 public class SkateboardTeleOp extends OpMode
 {
-    float DEAD_ZONE = 0.15f;
-    DcMotor backLeftMotor;
-    DcMotor backRightMotor;
-    DcMotor frontLeftMotor;
-    DcMotor frontRightMotor;
-    double backLeftPower   = 0;
-    double backRightPower  = 0;
-    double frontLeftPower  = 0;
-    double frontRightPower = 0;
-    Servo servo;
-    Servo left;
-    Servo right;
-    Servo pusherr;
-    I2cDevice range;
-    I2cDeviceReader rangeReader;
-    byte rangeReadings[];
-    boolean tog2 = false;
-    boolean xPressed = false;
-    boolean xPrevious = false;
-    boolean bPressed = false;
-    boolean bPrevious = false;
-    boolean aPressed = false;
-    boolean aPrevious = false;
-
-    double speedMultiplier;
-    float rightTrigger;
-
-
+    SteelSerpentsRobot robot;
     @Override
     public void init()
     {
-        servo = hardwareMap.servo.get("servo");
-        left = hardwareMap.servo.get("left");
-        right = hardwareMap.servo.get("right");
-        pusherr = hardwareMap.servo.get("pusherr");
-        backLeftMotor = hardwareMap.dcMotor.get("backl");
-        backRightMotor = hardwareMap.dcMotor.get("backr");
-        frontLeftMotor = hardwareMap.dcMotor.get("frontl");
-        frontRightMotor = hardwareMap.dcMotor.get("frontr");
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        left.setPosition(1);
-        right.setPosition(0);
-        pusherr.setPosition(0);
-        I2cDevice range;
-        range = hardwareMap.i2cDevice.get("range");
-        rangeReader = new I2cDeviceReader(range, new I2cAddr(0x28), 0x04, 2);
-        byte rangeReadings[];
+        robot = new SteelSerpentsRobot(hardwareMap);
     }
     @Override
     public void loop()
     {
-//        getGamepadValues();
-//        convertGamepadToMovement();
-        getMotorValues();
-        runMotors();
+        gamePad1Control();
     }
 
-    public void getGamepadValues()
+    /**
+     * Processes the inputs from gamepad1 and converts them into controls to drive the robot.
+     */
+    public void gamePad1Control()
     {
-        rightTrigger = gamepad1.right_trigger;
-        speedMultiplier = rightTrigger / 2;
-        print("SpeedMult", speedMultiplier);
-    }
-
-    public void getMotorValues()
-    {
-        double robotVelocity = 0;
-        double heading = 0;
+        double robotVelocity;
+        double heading;
         robotVelocity = gamepad1.left_trigger - gamepad1.right_trigger;
-        if (1 < robotVelocity)
+        robotVelocity = robot.clamp(robotVelocity);
+        heading = joystickToRadians(gamepad1.left_stick_x,gamepad1.left_stick_y);
+        robot.setMotorValues(robotVelocity, heading, gamepad1.right_stick_x);
+        robot.drive();
+    }
+
+    /**
+     * This function converts a joystick's direction into a heading in radians. TODO: Test this, determine what direction is zero, and which direction is positive.
+     * @param stickX The joystick's x value.
+     * @param stickY The joystick's y value.
+     * @return The joystick's heading in radians.
+     */
+    public double joystickToRadians(float stickX, float stickY)
+    {
+        double radians = 0;
+        if (stickX == 0 && 0 <= stickY)
         {
-            robotVelocity = 1;
+            radians = (3*Math.PI)/2;
         }
-        else if (robotVelocity < -1)
+        else if (stickX == 0 && stickY <= 0)
         {
-            robotVelocity = -1;
-        }
-        double leftStickY = gamepad1.left_stick_y;
-        double leftStickX = gamepad1.left_stick_x;
-        if (leftStickX == 0 && 0 <= leftStickY)
-        {
-            heading = (3*Math.PI)/2;
-        }
-        else if (leftStickX == 0 && leftStickY <= 0)
-        {
-            heading = (3*Math.PI)/2;
+            radians = (3*Math.PI)/2;
         }
         else
         {
-            heading = Math.atan(leftStickY/leftStickX);
-            if (leftStickX < 0) {
-                heading += Math.PI;
+            radians = Math.atan(stickY/stickX);
+            if (stickX < 0) {
+                radians += Math.PI;
             }
         }
-        heading += Math.PI/2;
-        heading %= 2*Math.PI;
-        frontLeftPower  = getFrontLeftMecanumVelocity(  robotVelocity, -heading, -gamepad1.right_stick_x);
-        frontRightPower = getFrontRightMecanumVelocity( robotVelocity, -heading, -gamepad1.right_stick_x);
-        backLeftPower   = getBackLeftMecanumVelocity(   robotVelocity, -heading, -gamepad1.right_stick_x);
-        backRightPower  = getBackRightMecanumVelocity(  robotVelocity, -heading, -gamepad1.right_stick_x);
-    }
-    public void convertGamepadToMovement()
-    {
-        // numOfMovements is a counter that counts how many directions that the rover is being pushed in.
-        // We use it to enable us to compound rover controls (move forward while turning, etc)
-        double numOfMovements = 0;
-        // Initialize drive power, this will be modified
-        frontLeftPower  = 0.0;
-        frontRightPower = 0.0;
-        backLeftPower   = 0.0;
-        backRightPower  = 0.0;
-        // If the left stick is pushed forward or back more than the DEAD_ZONE
-        if (DEAD_ZONE < Math.abs(gamepad1.left_stick_y))
-        {
-            // Drive Forward or Backward
-            frontLeftPower  += gamepad1.left_stick_y;
-            frontRightPower += gamepad1.left_stick_y;
-            backLeftPower   += gamepad1.left_stick_y;
-            backRightPower  += gamepad1.left_stick_y;
-            numOfMovements++;
-        }
-        // If the left stick is pushed left or right more than the DEAD_ZONE
-        if (DEAD_ZONE < Math.abs(gamepad1.left_stick_x))
-        {
-            // Strafe Left or Right
-            frontLeftPower  += gamepad1.left_stick_x;
-            frontRightPower += -gamepad1.left_stick_x;
-            backLeftPower   += -gamepad1.left_stick_x;
-            backRightPower  += gamepad1.left_stick_x;
-            numOfMovements++;
-        }
-        // If the left stick is in the center
-        if (Math.abs(gamepad1.left_stick_x) < DEAD_ZONE && Math.abs(gamepad1.left_stick_y) < DEAD_ZONE)
-        {
-            // Stop
-            frontLeftPower  = 0.0;
-            frontRightPower = 0.0;
-            backLeftPower   = 0.0;
-            backRightPower  = 0.0;
-        }
-
-
-        if (numOfMovements != 0) {
-            frontLeftPower  /= numOfMovements;
-            frontRightPower /= numOfMovements;
-            backLeftPower   /= numOfMovements;
-            backRightPower  /= numOfMovements;
-        }
-
-        frontLeftPower  *= speedMultiplier;
-        frontRightPower *= speedMultiplier;
-        backLeftPower   *= speedMultiplier;
-        backRightPower  *= speedMultiplier;
-    }
-
-
-    /**
-     * Gets the speed for the front left wheel.
-     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
-     */
-    public double getFrontLeftMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
-    {
-        return robotVelocity * Math.sin(robotHeading + (Math.PI / 4)) + robotAngularVelocity;
-    }
-    /**
-     * Gets the speed for the front right wheel.
-     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
-     */
-    public double getFrontRightMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
-    {
-        return robotVelocity * Math.cos(robotHeading + (Math.PI / 4)) - robotAngularVelocity;
-    }
-    /**
-     * Gets the speed for the back left wheel.
-     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
-     */
-    public double getBackLeftMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
-    {
-        return robotVelocity * Math.cos(robotHeading + (Math.PI / 4)) + robotAngularVelocity;
-    }
-    /**
-     * Gets the speed for the back rightwheel.
-     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
-     */
-    public double getBackRightMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
-    {
-        return robotVelocity * Math.sin(robotHeading + (Math.PI / 4)) - robotAngularVelocity;
-    }
-
-    public void runMotors()
-    {
-        frontLeftPower  = clamp(frontLeftPower  );
-        frontRightPower = clamp(frontRightPower );
-        backLeftPower   = clamp(backLeftPower   );
-        backRightPower  = clamp(backRightPower  );
-        frontLeftMotor.setPower(    frontLeftPower  );
-        frontRightMotor.setPower(   frontRightPower );
-        backLeftMotor.setPower(     backLeftPower   );
-        backRightMotor.setPower(    backRightPower  );
-    }
-    public double clamp(double value)
-    {
-        if (value < -1)
-        {
-            print("Clamp", value);
-            value = -1;
-        }
-        else if (1 < value)
-        {
-            print("Clamp", value);
-            value = 1;
-        }
-        return value;
+        radians += Math.PI/2;
+        radians %= 2*Math.PI;
+        return radians;
     }
     @Override
     public void stop()
---
+++ SteelSerpentsRobot.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+import android.util.Log;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceReader;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * <h1>SteelSerpentsRobot</h1>
+ * The robot class used by the SteelSerpents FTC team. This class contains all of the robot hardware,
+ * drive instructions, and other details specifically relating to controlling the robot.
+ */
+public class SteelSerpentsRobot
+{
+
+    float DEAD_ZONE = 0.15f;
+    HardwareMap hardwareMap;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    double backLeftPower   = 0;
+    double backRightPower  = 0;
+    double frontLeftPower  = 0;
+    double frontRightPower = 0;
+    Servo servo;
+    Servo left;
+    Servo right;
+    Servo pusherr;
+    Servo pusherl;
+    I2cDevice range;
+    I2cDeviceReader rangeReader;
+    byte rangeReadings[];
+
+    /**
+     * Sets up the SteelSerpents robot by initializing its hardware.
+     * @param newHardwareMap The robot HardwareMap that is provided by our robot's OpMode.
+     */
+    SteelSerpentsRobot(HardwareMap newHardwareMap)
+    {
+        this.hardwareMap = newHardwareMap;
+        servo = hardwareMap.servo.get("servo");
+        left = hardwareMap.servo.get("left");
+        right = hardwareMap.servo.get("right");
+        pusherr = hardwareMap.servo.get("pusherr");
+        pusherl = hardwareMap.servo.get("pusherl");
+        backLeftMotor = hardwareMap.dcMotor.get("backl");
+        backRightMotor = hardwareMap.dcMotor.get("backr");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontl");
+        frontRightMotor = hardwareMap.dcMotor.get("frontr");
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        left.setPosition(1);
+        right.setPosition(0);
+        pusherr.setPosition(0);
+        I2cDevice range;
+        range = hardwareMap.i2cDevice.get("range");
+        rangeReader = new I2cDeviceReader(range, new I2cAddr(0x28), 0x04, 2);
+        byte rangeReadings[];
+    }
+    /**
+     * Gets the speed for the front left wheel.
+     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
+     */
+    private double getFrontLeftMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
+    {
+        return robotVelocity * Math.sin(robotHeading + (Math.PI / 4)) + robotAngularVelocity;
+    }
+    /**
+     * Gets the speed for the front right wheel.
+     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
+     */
+    private double getFrontRightMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
+    {
+        return robotVelocity * Math.cos(robotHeading + (Math.PI / 4)) - robotAngularVelocity;
+    }
+    /**
+     * Gets the speed for the back left wheel.
+     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
+     */
+    private double getBackLeftMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
+    {
+        return robotVelocity * Math.cos(robotHeading + (Math.PI / 4)) + robotAngularVelocity;
+    }
+    /**
+     * Gets the speed for the back rightwheel.
+     * Based on this paper <a href="http://thinktank.wpi.edu/resources/346/ControllingMecanumDrive.pdf">Controlling Mecanum Drive</a>
+     */
+    private double getBackRightMecanumVelocity(double robotVelocity, double robotHeading, double robotAngularVelocity)
+    {
+        return robotVelocity * Math.sin(robotHeading + (Math.PI / 4)) - robotAngularVelocity;
+    }
+
+    /**
+     * Sets the motor speeds to the appropriate values for a mecanum-wheeled robot. Must use the drive() command to send the drive instruction to the robot.
+     * @param robotVelocity The speed at which you want the robot to translate over the gamefield. Range: [-1,1]
+     * @param directionOfMovement The heading in radians in which you want the robot to translate. Range: [0,2PI)
+     * @param rateOfRotation The rate of rotation and direction of rotation you wish to move in. Range: [-1,1]
+     */
+    public void setMotorValues(double robotVelocity, double directionOfMovement, double rateOfRotation)
+    {
+        frontLeftPower  = getFrontLeftMecanumVelocity(  robotVelocity, -directionOfMovement, -rateOfRotation);
+        frontRightPower = getFrontRightMecanumVelocity( robotVelocity, -directionOfMovement, -rateOfRotation);
+        backLeftPower   = getBackLeftMecanumVelocity(   robotVelocity, -directionOfMovement, -rateOfRotation);
+        backRightPower  = getBackRightMecanumVelocity(  robotVelocity, -directionOfMovement, -rateOfRotation);
+    }
+
+    /**
+     * Sends the actual drive command to the robot.
+     */
+    public void drive()
+    {
+        frontLeftPower  = clamp(frontLeftPower  );
+        frontRightPower = clamp(frontRightPower );
+        backLeftPower   = clamp(backLeftPower   );
+        backRightPower  = clamp(backRightPower  );
+        frontLeftMotor.setPower(    frontLeftPower  );
+        frontRightMotor.setPower(   frontRightPower );
+        backLeftMotor.setPower(     backLeftPower   );
+        backRightMotor.setPower(    backRightPower  );
+    }
+    public double clamp(double value, int min, int max)
+    {
+        if (value < min)
+        {
+            print("Clamp", value);
+            value = min;
+        }
+        else if (max < value)
+        {
+            print("Clamp", value);
+            value = max;
+        }
+        return value;
+    }
+    public double clamp (double value)
+    {
+        return clamp(value, -1, 1);
+    }
+    public void print(String key, String text)
+    {
+        Log.d(key, text);
+    }
+    public void print(String key, double text)
+    {
+        print(key, String.valueOf(text));
+    }
+}
